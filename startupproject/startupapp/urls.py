1aa0732cc45711e97d628c525b55fe9475901f42
thepicture <85333076+thepicture@users.noreply.github.com>
2021-11-11 10:33:55 +0000

    Navbar and header are done

--- startupproject/startupapp/urls.py
+++ startupproject/startupapp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import PublisherList
+from .views import HomeView, PublisherList
 
 urlpatterns = [
+    url(r'^home/$', HomeView.as_view(), name='home-view'),
     url(r'^startups/$', PublisherList.as_view(), name='startup-list')
 ]
--- startupproject/startupapp/views.py
+++ startupproject/startupapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import StartUp
-from django.views.generic import ListView
+from django.views.generic import ListView, View
 
 
 class PublisherList(ListView):
     model = StartUp
+
+class HomeView(View):
+    template_name = ''
